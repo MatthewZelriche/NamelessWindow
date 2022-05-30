f24cfca25d25bb650a26660a5a41053acfeda414
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2022-05-30 13:51:55 -0600

    Implement secondary window in demo.

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -14,6 +14,60 @@
 #include "../Extra/imgui_impl_nlswin.hpp"
 #include "imgui/backends/imgui_impl_opengl3.h"
 
+void WindowHeader(std::shared_ptr<NLSWIN::Window> window, int id) {
+   ImGui::PushID(id);
+   ImGuiIO &io = ImGui::GetIO();
+   ImGui::Text("Generic Window ID: %d", window->GetGenericID());
+   ImGui::Text("Frametime: %fms", io.DeltaTime);
+   ImGui::Separator();
+   int pos[2];
+   NLSWIN::Point windowPos = window->GetWindowPos();
+
+   pos[0] = windowPos.x;
+   pos[1] = windowPos.y;
+   ImGui::InputInt2("Position", pos);
+   if (ImGui::IsItemDeactivatedAfterEdit()) {
+      window->Reposition(pos[0], pos[1]);
+   }
+   int res[2];
+   res[0] = window->GetWindowWidth();
+   res[1] = window->GetWindowHeight();
+   ImGui::InputInt2("Resolution", res);
+   bool resChanged = ImGui::IsItemDeactivatedAfterEdit();
+   if (resChanged) {
+      try {
+         window->Resize(res[0], res[1]);
+      } catch (NLSWIN::InvalidVideoModeException e) { ImGui::OpenPopup("Invalid Video Mode"); }
+   }
+   bool isBorderless = window->IsBorderless();
+   if (ImGui::Checkbox("Borderless", &isBorderless)) {
+      if (isBorderless) {
+         window->EnableBorderless();
+      } else {
+         window->DisableBorderless();
+      }
+   }
+   int index = (int)window->GetWindowMode();
+   ImGui::Combo("Mode", &index, "Fullscreen\0Windowed\0");
+
+   NLSWIN::WindowMode desiredMode = (NLSWIN::WindowMode)index;
+   if (desiredMode != window->GetWindowMode()) {
+      switch (desiredMode) {
+         case NLSWIN::WindowMode::WINDOWED: {
+            window->SetWindowed();
+            break;
+         }
+         case NLSWIN::WindowMode::FULLSCREEN: {
+            try {
+               window->SetFullscreen();
+            } catch (...) { ImGui::OpenPopup("Invalid Video Mode"); }
+            break;
+         }
+      }
+   }
+   ImGui::PopID();
+}
+
 int main() {
    NLSWIN::WindowProperties props {0};
    props.horzResolution = 1280;
@@ -27,6 +81,9 @@ int main() {
    auto context = NLSWIN::GLContext::Create(win);
    win->Show();
    kb->SubscribeToWindow(win);
+
+   std::shared_ptr<NLSWIN::Window> win2 = nullptr;
+   std::shared_ptr<NLSWIN::GLContext> context2 = nullptr;
 
    // Load opengl
    context->MakeContextCurrent();
@@ -49,6 +106,15 @@ int main() {
    while (!win->RequestedClose()) {
       NLSWIN::EventBus::PollEvents();
 
+      if (win2 && win2->RequestedClose()) {
+         win2 = nullptr;
+      }
+
+      while (win2 && win2->HasEvent()) {
+         auto evt = win2->GetNextEvent();
+         ImGui_ImplNLSWin_HandleEvent(evt);
+      }
+
       while (win && win->HasEvent()) {
          auto evt = win->GetNextEvent();
          ImGui_ImplNLSWin_HandleEvent(evt);
@@ -68,55 +134,39 @@ int main() {
 
       ImGui::SetNextWindowPos(ImVec2 {0, 0});
       ImGui::SetNextWindowSize(ImVec2 {350, (float)win->GetWindowHeight()});
-      ImGui::Begin("Interactive NLSWIN Demo", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
-      bool resChanged = false;
-      if (ImGui::CollapsingHeader("Primary Window Information:", ImGuiTreeNodeFlags_DefaultOpen)) {
-         ImGui::Text("Generic Window ID: %d", win->GetGenericID());
-         ImGui::Text("Frametime: %fms", io.DeltaTime);
-         ImGui::Separator();
-         int pos[2];
-         NLSWIN::Point windowPos = win->GetWindowPos();
+      ImGui::Begin("Interactive NLSWIN Demo", nullptr,
+                   ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
 
-         pos[0] = windowPos.x;
-         pos[1] = windowPos.y;
-         ImGui::InputInt2("Position", pos);
-         if (ImGui::IsItemDeactivatedAfterEdit()) {
-            win->Reposition(pos[0], pos[1]);
-         }
-         int res[2];
-         res[0] = win->GetWindowWidth();
-         res[1] = win->GetWindowHeight();
-         ImGui::InputInt2("Resolution", res);
-         resChanged = ImGui::IsItemDeactivatedAfterEdit();
-         if (resChanged) {
-            try {
-               win->Resize(res[0], res[1]);
-            } catch (NLSWIN::InvalidVideoModeException e) { ImGui::OpenPopup("Invalid Video Mode"); }
-         }
-         bool isBorderless = win->IsBorderless();
-         if (ImGui::Checkbox("Borderless", &isBorderless)) {
-            if (isBorderless) {
-               win->EnableBorderless();
+      if (ImGui::CollapsingHeader("Primary Window: ", ImGuiTreeNodeFlags_DefaultOpen)) {
+         WindowHeader(win, 1);
+      }
+
+      if (ImGui::CollapsingHeader("Secondary Window: ", ImGuiTreeNodeFlags_DefaultOpen)) {
+         if (ImGui::Button("Create", {150, 25})) {
+            if (win2) {
+               ImGui::OpenPopup("Already Created");
             } else {
-               win->DisableBorderless();
+               NLSWIN::WindowProperties props;
+               props.yCoordinate = 50;
+               props.windowName = "Secondary window";
+               win2 = NLSWIN::Window::Create(props);
+               win2->Show();
+               context2 = NLSWIN::GLContext::Create(win2);
             }
          }
-         int index = (int)win->GetWindowMode();
-         ImGui::Combo("Mode", &index, "Fullscreen\0Windowed\0");
+         ImGui::SameLine();
+         if (ImGui::Button("Destroy", {150, 25})) {
+            if (win2) {
+               win2 = nullptr;
+            } else {
+               ImGui::OpenPopup("Doesn't Exist");
+            }
+         }
 
-         NLSWIN::WindowMode desiredMode = (NLSWIN::WindowMode)index;
-         if (desiredMode != win->GetWindowMode()) {
-            switch (desiredMode) {
-               case NLSWIN::WindowMode::WINDOWED: {
-                  win->SetWindowed();
-                  break;
-               }
-               case NLSWIN::WindowMode::FULLSCREEN: {
-                  try {
-                     win->SetFullscreen();
-                  } catch (...) { ImGui::OpenPopup("Invalid Video Mode"); }
-                  break;
-               }
+         if (win2) {
+            WindowHeader(win2, 2);
+            if (ImGui::Button("Focus", {175, 25})) {
+               win2->Focus();
             }
          }
       }
@@ -128,8 +178,15 @@ int main() {
          ImGui::Text("The requested fullscreen resolution is invalid. \n");
          ImGui::EndPopup();
       }
+      if (ImGui::BeginPopupModal("Already Created", &alwaysTrue)) {
+         ImGui::Text("Cannot instantiate second window more than once. \n");
+         ImGui::EndPopup();
+      }
 
-      if (ImGui::CollapsingHeader("Secondary Window")) {}
+      if (ImGui::BeginPopupModal("Doesn't Exist", &alwaysTrue)) {
+         ImGui::Text("Cannot destroy secondary window as it does not exist. \n");
+         ImGui::EndPopup();
+      }
 
       ImGui::End();
 
@@ -138,6 +195,14 @@ int main() {
       glad_glClearColor(0.4, 0.4, 0.4, 1);
       ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
       context->SwapContextBuffers();
+
+      if (win2) {
+         context2->MakeContextCurrent();
+         glad_glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+         glad_glClearColor(0.39, 0.584, 0.929, 1);
+         context2->SwapContextBuffers();
+         context->MakeContextCurrent();
+      }
    }
 
    ImGui_ImplNLSWin_Shutdown();
